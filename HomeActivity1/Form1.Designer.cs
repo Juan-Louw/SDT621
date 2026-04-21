f346467f0088127b54fa5b7eb4168a6b46574202
Juan <juanlouw209@gmail.com>
2026-04-21 16:52:58 +0200

    HomeActivity1

--- HomeActivity1/Form1.Designer.cs
+++ HomeActivity1/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            txtNumber = new TextBox();
+            btnPlay = new Button();
+            lblResults = new Label();
+            lblAttempts = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(241, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(44, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Lottery";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(114, 46);
+            label2.Name = "label2";
+            label2.Size = new Size(171, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Enter your lucky number (1-20)";
+            // 
+            // txtNumber
+            // 
+            txtNumber.Location = new Point(303, 43);
+            txtNumber.Name = "txtNumber";
+            txtNumber.Size = new Size(100, 23);
+            txtNumber.TabIndex = 2;
+            // 
+            // btnPlay
+            // 
+            btnPlay.Location = new Point(114, 87);
+            btnPlay.Name = "btnPlay";
+            btnPlay.Size = new Size(289, 23);
+            btnPlay.TabIndex = 3;
+            btnPlay.Text = "Play";
+            btnPlay.UseVisualStyleBackColor = true;
+            btnPlay.Click += btnPlay_Click;
+            // 
+            // lblResults
+            // 
+            lblResults.AutoSize = true;
+            lblResults.Location = new Point(114, 122);
+            lblResults.Name = "lblResults";
+            lblResults.Size = new Size(38, 15);
+            lblResults.TabIndex = 4;
+            lblResults.Text = "label3";
+            // 
+            // lblAttempts
+            // 
+            lblAttempts.AutoSize = true;
+            lblAttempts.Location = new Point(409, 95);
+            lblAttempts.Name = "lblAttempts";
+            lblAttempts.Size = new Size(38, 15);
+            lblAttempts.TabIndex = 5;
+            lblAttempts.Text = "label3";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(569, 305);
+            Controls.Add(lblAttempts);
+            Controls.Add(lblResults);
+            Controls.Add(btnPlay);
+            Controls.Add(txtNumber);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtNumber;
+        private Button btnPlay;
+        private Label lblResults;
+        private Label lblAttempts;
     }
 }
--- HomeActivity1/Form1.cs
+++ HomeActivity1/Form1.cs
@@ -2,9 +2,65 @@ namespace HomeActivity1
 {
     public partial class Form1 : Form
     {
+        readonly Random random = new Random();
+        int winNumber;
+        int attemptsLeft;
+        const int MaxAttempts = 3;
+
         public Form1()
         {
             InitializeComponent();
+            StartNewGame();
+        }
+
+        private void StartNewGame() 
+        {
+            winNumber = random.Next(1, 21);
+            attemptsLeft = MaxAttempts;
+            lblAttempts.Text = $"Attempts left: {attemptsLeft}";
+            lblResults.Text = string.Empty;
+            btnPlay.Enabled = true;
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            int number;
+            try
+            {
+                number = int.Parse(txtNumber.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter a valid number between 1 and 20.");
+                return;
+            }
+
+            if (number < 1 || number > 20)
+            {
+                MessageBox.Show("Please enter a valid number between 1 and 20.");
+                return;
+            }
+
+            if (number == winNumber)
+            {
+                lblResults.Text = "Jackpot Winner!";
+                btnPlay.Enabled = false;
+            }
+            else
+            {
+                attemptsLeft--;
+                if (attemptsLeft > 0)
+                {
+                    lblResults.Text = "Try Again!";
+                    lblAttempts.Text = $"Attempts left: {attemptsLeft}";
+                }
+                else
+                {
+                    lblResults.Text = $"No attempts left. Winning number was {winNumber}.";
+                    lblAttempts.Text = "Attempts left: 0";
+                    btnPlay.Enabled = false;
+                }
+            }
         }
     }
 }
